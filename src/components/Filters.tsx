f9de91a0e3efde660fc542b9cb3cf5d6d16d4c5a
TarunITParishrama <it35.parishrama@gmail.com>
2025-07-17 14:39:30 +0530

    Updated Filters.tsx 17Jul, 2.40pm

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -21,7 +21,7 @@ export default function Filters({ onFilterChange }: FiltersProps) {
   console.log(BASE_URL);
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/api/getDistinctAreas`)
+      .get(`${BASE_URL}/api/getdistinctareas`)
       .then((res) => {
         const sortedPanchayats = (res.data.panchayats || []).sort(
           (a: string, b: string) => a.localeCompare(b)
